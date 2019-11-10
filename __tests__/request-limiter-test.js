05eb2107c91c91c21655641660d6c4e739fd49fa
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2019-11-10 14:52:56 +0200

    add delay middleware

--- __tests__/request-limiter-test.js
+++ __tests__/request-limiter-test.js
@@ -1,49 +1,88 @@
 const request = require('supertest');
-const app = require('../utils/server');
+const { initApp, delayMiddleware } = require('../utils/server');
 let server;
 
 
 describe('Test the root path', () => {
-    beforeAll(() => {
-        server = app.listen(3000, () => {
-            console.log('Server is listening on port 3000!');
+    describe('It should init server and check all routes', () => {
+        beforeAll(() => {
+            server = initApp().listen(3000, () => {
+                console.log('Server is listening on port 3000!');
+            });
         });
-    });
 
-    afterAll(() => {
-        if (server) {
-            server.close();
-            console.log('The server is closed');
-        }
-    });
+        afterAll(() => {
+            if (server) {
+                server.close();
+                console.log('The server is closed');
+            }
+        });
 
-    describe('It should response by the CRUD methods from server routes', ()  => {
         test('It should response the GET method from /path_one', async () => {
-            const response = await request(app).get('/path_one');
+            const response = await request(server).get('/path_one');
             expect(response.statusCode).toBe(200);
         });
 
         test('It should response the POST method from /path_one', async () => {
-            const response = await request(app).post('/path_one', {});
+            const response = await request(server).post('/path_one', {});
             expect(response.statusCode).toBe(200);
         });
 
         test('It should response the PUT method from /path_one', async () => {
-            const response = await request(app).put('/path_one', {});
+            const response = await request(server).put('/path_one', {});
             expect(response.statusCode).toBe(200);
         });
 
         test('It should response the GET method from /path_two', async () => {
-            const response = await request(app).get('/path_two');
+            const response = await request(server).get('/path_two');
             expect(response.statusCode).toBe(200);
         });
 
         test('It should response the DELETE method from /path_two', async () => {
-            const response = await request(app).del('/path_two');
+            const response = await request(server).del('/path_two');
             expect(response.statusCode).toBe(204);
         });
-    })
+    });
 
+    describe('It should init server with delay middleware and check all routes', () => {
+        beforeAll(() => {
+            server = initApp([delayMiddleware(200)]).listen(3000, () => {
+                console.log('Server is listening on port 3000!');
+            });
+        });
+
+        afterAll(() => {
+            if (server) {
+                server.close();
+                console.log('The server is closed');
+            }
+        });
+
+        test('It should response the GET method from /path_one', async () => {
+            const response = await request(server).get('/path_one');
+            expect(response.statusCode).toBe(200);
+        });
+
+        test('It should response the POST method from /path_one', async () => {
+            const response = await request(server).post('/path_one', {});
+            expect(response.statusCode).toBe(200);
+        });
+
+        test('It should response the PUT method from /path_one', async () => {
+            const response = await request(server).put('/path_one', {});
+            expect(response.statusCode).toBe(200);
+        });
+
+        test('It should response the GET method from /path_two', async () => {
+            const response = await request(server).get('/path_two');
+            expect(response.statusCode).toBe(200);
+        });
+
+        test('It should response the DELETE method from /path_two', async () => {
+            const response = await request(server).del('/path_two');
+            expect(response.statusCode).toBe(204);
+        });
+    });
 
 });
 
--- utils/server.js
+++ utils/server.js
@@ -1,27 +1,46 @@
 const express = require('express');
 
+function initApp(middlewaresList = []) {
+    const app = express();
 
-const app = express();
+    if (Array.isArray(middlewaresList) && middlewaresList.length > 0) {
+        for (let i = 0; i < middlewaresList.length; i += 1) {
+            app.use(middlewaresList[i])
+        }
+    }
 
-app.get('/path_one', (req, res, next) => {
-    res.send('Hello from GET path_one route')
-});
+    app.get('/path_one', (req, res, next) => {
+        res.send('Hello from GET path_one route')
+    });
 
-app.post('/path_one', (req, res, next) => {
-    res.send('Hello from POST path_one route')
-});
+    app.post('/path_one', (req, res, next) => {
+        res.send('Hello from POST path_one route')
+    });
 
-app.put('/path_one', (req, res, next) => {
-    res.send('Hello from PUT path_one route')
-});
+    app.put('/path_one', (req, res, next) => {
+        res.send('Hello from PUT path_one route')
+    });
 
-app.get('/path_two', (req, res, next) => {
-    res.send('Hello from GET path_two route')
-});
+    app.get('/path_two', (req, res, next) => {
+        res.send('Hello from GET path_two route')
+    });
 
-app.del('/path_two', (req, res, next) => {
-    res.status(204).send('Hello from DEL path_two route')
-});
+    app.del('/path_two', (req, res, next) => {
+        res.status(204).send('Hello from DEL path_two route')
+    });
+
+    return app;
+}
 
 
-module.exports = app;
+function delayMiddleware(value = 1000) {
+    return function(req, res, next) {
+        setTimeout(() => {
+            next();
+        }, value)
+    }
+}
+
+
+module.exports.initApp = initApp;
+module.exports.delayMiddleware = delayMiddleware;
