fbe13740e2da5a2313f5315e1ac1c3f5aba99dd8
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2019-11-10 15:37:21 +0200

    add request limiter tests

--- __tests__/request-limiter-test.js
+++ __tests__/request-limiter-test.js
@@ -1,7 +1,9 @@
 const request = require('supertest');
 const { initApp, delayMiddleware } = require('../utils/server');
-let server;
+const RequestLimiter = require('../index');
 
+let server;
+const delay = 500; // ms
 
 describe('Test the root path', () => {
     describe('It should init server and check all routes', () => {
@@ -46,7 +48,7 @@ describe('Test the root path', () => {
 
     describe('It should init server with delay middleware and check all routes', () => {
         beforeAll(() => {
-            server = initApp([delayMiddleware(200)]).listen(3000, () => {
+            server = initApp([delayMiddleware(delay)]).listen(3000, () => {
                 console.log('Server is listening on port 3000!');
             });
         });
@@ -83,6 +85,100 @@ describe('Test the root path', () => {
             expect(response.statusCode).toBe(204);
         });
     });
+
+    describe('It should init server with requestLimiter and add Response headers', () => {
+        let maxRequests = 2;
+        beforeAll(() => {
+            const requestLimiter = RequestLimiter({
+                maxRequests,
+                headers: true,
+                routesList: [{ path: '/path_one', method: 'GET' }, { path: 'path_two', method: 'DELETE' }],
+            });
+
+            server = initApp([requestLimiter]).listen(3000, () => {
+                console.log('Server is listening on port 3000!');
+            });
+        });
+
+        afterAll(() => {
+            if (server) {
+                server.close();
+                console.log('The server is closed');
+            }
+        });
+
+        test('It should response with additional headers', async () => {
+            const response = await request(server).get('/path_one');
+            expect(response.headers).toHaveProperty('x-requestlimit-limit');
+            expect(response.headers).toHaveProperty('x-requestlimit-usage');
+            expect(response.headers['x-requestlimit-limit']).toEqual(maxRequests.toString());
+            expect(response.headers['x-requestlimit-usage']).toEqual('0');
+            expect(response.statusCode).toBe(200);
+        });
+    });
+
+    describe('It should init server with requestLimiter and not add Response headers', () => {
+        let maxRequests = 2;
+        beforeAll(() => {
+            const requestLimiter = RequestLimiter({
+                maxRequests,
+                headers: false,
+                routesList: [{ path: '/path_one', method: 'GET' }, { path: 'path_two', method: 'DELETE' }],
+            });
+
+            server = initApp([requestLimiter]).listen(3000, () => {
+                console.log('Server is listening on port 3000!');
+            });
+        });
+
+        afterAll(() => {
+            if (server) {
+                server.close();
+                console.log('The server is closed');
+            }
+        });
+
+        test('It should response with no additional headers', async () => {
+            const response = await request(server).get('/path_one');
+            expect(response.headers).not.toHaveProperty('x-requestlimit-limit');
+            expect(response.headers).not.toHaveProperty('x-requestlimit-usage');
+            expect(response.statusCode).toBe(200);
+        });
+    });
+
+    describe('It should init server with requestLimiter', () => {
+        beforeAll(() => {
+            const requestLimiter = RequestLimiter({
+                maxRequests: 2,
+                headers: true,
+                routesList: [{ path: '/path_one', method: 'GET' }, { path: 'path_two', method: 'DELETE' }],
+            });
+
+            server = initApp([requestLimiter, delayMiddleware(delay)]).listen(3000, () => {
+                console.log('Server is listening on port 3000!');
+            });
+        });
+
+        afterAll(() => {
+            if (server) {
+                server.close();
+                console.log('The server is closed');
+            }
+        });
+
+        test('It should response with error for requests number more then max', async () => {
+            const responseOne = request(server).get('/path_one');
+            const responseTwo = request(server).get('/path_one');
+            const responseThree = request(server).get('/path_one');
+            await Promise.all([responseOne, responseTwo, responseThree]).then(([_resOne, _resTwo, _resThree]) => {
+                expect(_resOne.statusCode).toBe(200);
+                expect(_resTwo.statusCode).toBe(200);
+                expect(_resThree.statusCode).toBe(429);
+            });
+        });
+    });
+
+
 
 });
 
